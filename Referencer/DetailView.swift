e80be63c887f2520261e370b11a52ba5b3489867
William Dower <william.dower@griffithuni.edu.au>
2020-03-05 10:19:13 +1000

    Created DetailView.swift with DetailView class, which will display a single game and its details

---
+++ Referencer/DetailView.swift
@@ -0,0 +1,17 @@
+//
+//  DetailView.swift
+//  Referencer
+//
+//  Created by William Dower on 5/3/20.
+//  Copyright © 2020 William Dower. All rights reserved.
+//
+
+import Foundation
+
+class DetailView {
+    let game: VideoGame
+    
+    init(game: VideoGame) {
+        self.game = game
+    }
+}
