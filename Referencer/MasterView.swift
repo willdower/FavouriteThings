8038e61da72291fc29116826827847c1e59e5bb7
William Dower <william.dower@griffithuni.edu.au>
2020-03-14 16:12:06 +1000

    Made previews and list for MasterView

--- Referencer/MasterView.swift
+++ Referencer/MasterView.swift
@@ -8,15 +8,23 @@
 
 import SwiftUI
 
+import SwiftUI
+
 struct MasterView: View {
-    var gameViewModel: VideoGameViewModel
+    var gamesList: GameList
     var body: some View {
-        DetailView(gameViewModel: gameViewModel)
+        List {
+            ForEach(gamesList.games, id: \.title) { game in
+            NavigationLink(destination: DetailView(gameViewModel: VideoGameViewModel(game: game))) {
+                Text("Test")
+                }
+            }
+        }
     }
 }
 
 struct MasterView_Previews: PreviewProvider {
     static var previews: some View {
-        MasterView(gameViewModel: VideoGameViewModel(game: VideoGame(title: "Minecraft", imageName: "minecraftArt", developer: "Mojang", releaseYear: 2009, releaseMonth: 5, releaseDay: 17, userRating: 7.7, criticRating: 93)))
+        MasterView(gamesList: GameList(games: [VideoGame(title: "Minecraft", imageName: "minecraftArt", developer: "Mojang", releaseYear: 2009, releaseMonth: 5, releaseDay: 17, userRating: 7.7, criticRating: 93), VideoGame(title: "Fortnite", imageName: "fortniteArt", developer: "Epic Games", releaseYear: 2017, releaseMonth: 7, releaseDay: 25, userRating: 2.9, criticRating: 81), VideoGame(title: "Steep", imageName: "steepArt", developer: "Ubisoft", releaseYear: 2016, releaseMonth: 12, releaseDay: 2, userRating: 6.8, criticRating: 72)], listTitle: "Video Games"))
     }
 }
